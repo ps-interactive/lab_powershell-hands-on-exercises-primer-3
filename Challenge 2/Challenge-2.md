c0e9701f0c30076dec842d3909d8cde6119dca1a
Liam Cleary <liamcleary@msn.com>
2023-11-22 21:44:39 -0500

    1. Updated the Challenge-2.xml file to include the new challenge 2. Also updated the Challenge-2.ps1 file to include the new challenge 3. Also updated the Challenge-2.md file

---
+++ Challenge 2/Challenge-2.md
@@ -0,0 +1,62 @@
+
+# Challenge: Generating an HTML Report from XML Data Using PowerShell in Visual Studio Code
+
+By the end of this challenge, you should be able to read and process XML data using PowerShell and then generate an HTML report from that data using Visual Studio Code.
+
+## Prerequisites
+
+* Basic understanding of PowerShell scripting.
+* Access to a Windows environment with Visual Studio Code installed.
+
+## Tasks
+
+1. **Launch Visual Studio Code as an Administrator**.
+   ![Right Click on Visual Studio Code to Launch as an Administrator](./Images/Challenge-1.0.png "Right Click on Visual Studio Code to Launch as an Administrator")
+
+2. **From the Visual Studio Code welcome screen, click the Open Folder option**.
+   ![Open a Folder by Clicking Open Folder on the Welcome Screen](./Images/Challenge-1.1.png "Open a Folder by Clicking Open Folder on the Welcome Screen")
+
+3. **Navigate to the LAB_FILES folder from the Desktop, and select Select Folder**.
+
+4. **In the left navigation, double-click the file `Challenge-2.ps1` to load it into the main window**.
+
+5. **Click Terminal from the top navigation within Visual Studio Code, and click New Terminal**.
+   ![Click Terminal to Launch a Terminal Window](./Images/Challenge-1.2.png "Click Terminal to Launch a Terminal Window")
+
+6. **Ensure the Terminal is set to PowerShell (pwsh) by checking the top of the Terminal which is below the main PowerShell script**.
+   ![Check the Terminal is PowerShell (pwsh)](./Images/Challenge-1.3.png "Check the Terminal is PowerShell (pwsh)")
+
+7. **Load the XML Data into PowerShell**:
+   - Locate the section starting with `[xml]$xmlData = Get-Content...`.
+   - Highlight this entire section.
+   - Right-click the highlighted text and select "Run Selection".
+   ![Run XML Parsing Section](./Images/Challenge-1.4.png "Run XML Parsing Section")
+
+8. **Convert Departments and Employees to HTML**:
+   - Find the section that starts with `function Convert-SectionToHtml...`.
+   - Highlight the entire function definition.
+   - Right-click the highlighted text and select "Run Selection".
+   ![Process Departments and Employees](./Images/Challenge-1.5.png "Process Departments and Employees")
+
+9. **Convert Products to HTML**:
+   - Highlight the lines that include `$productsHtml = Convert-SectionToHtml -SectionTitle "Products"...`.
+   - Right-click and choose "Run Selection".
+   ![Process Products](./Images/Challenge-1.6.png "Process Products")
+
+10. **Generate the Full HTML Report**:
+    - Highlight the section starting with `# Combine HTML content...` and ending with `$fullHtml | Out-File...`.
+    - Right-click and select "Run Selection" to execute.
+    ![Generate Full HTML Report](./Images/Challenge-1.7.png "Generate Full HTML Report")
+
+11. **Save the HTML Report**:
+    - Highlight the last line of the script: `$fullHtml | Out-File '.\Challenge 2\Challenge-2-All.html'`.
+    - Right-click and select "Run Selection" to save the file.
+    ![Save HTML Report](./Images/Challenge-1.8.png "Save HTML Report")
+
+12. **Validate the Report**:
+    - Open File Explorer and navigate to the `Challenge 2` directory.
+    - Locate and double-click `Challenge-2-All.html` to open it in your web browser.
+    - Verify the content of the HTML report against the XML data.
+    ![Open and Validate HTML Report](./Images/Challenge-1.9.png "Open and Validate HTML Report")
+
+Well done on completing this challenge! You've now learned how to process XML data with PowerShell and create an HTML report using Visual Studio Code.
--- Challenge 2/Challenge-2.ps1
+++ Challenge 2/Challenge-2.ps1
@@ -1,4 +1,162 @@
+# Load 'Challenge-2.xml' into a variable called $xmlData
+[xml]$xmlData = Get-Content '.\Challenge 2\Challenge-2.xml'
 
-[xml]$xmlData = Get-Content ./Challenge-2.xml
+# Display the Company Node
+$xmlData.company
+
+# Display the Departments Node
+$xmlData.company.departments
+
+# Iterate the Department Nodes
+$xmlData.company.departments.department
+
+# Display the Products Node
+$xmlData.company.products
+
+# Iterate the Product Nodes
+$xmlData.company.products.product
+
+# Iterate all Products with a price greater than 100
+$xmlData.company.products.product | `
+    Where-Object { [int]$_.price -lt 100 }
+
+# Iterate all Products with a price greater than 100
+$xmlData.company.products.product | `
+    Where-Object { [int]$_.'price' -gt 100 }
+
+# Iterate all Product Categories
+$xmlData.company.products.product | `
+Select-Object -ExpandProperty category | `
+    Sort-Object -Unique
+
+# Iterate all Products and Group by Category, then display the Category and the number of products per category
+$xmlData.company.products.product | `
+    Group-Object -Property category | `
+        Select-Object Name, Count
+
+# Retrieve all Products in the 'Computers' Category and Format as a Table
+$xmlData.company.products.product | `
+    Where-Object { $_.category -eq "Computers" } | `
+        Format-Table -AutoSize
+
+# Iterate all Products and Set the Foreground Color depending on their Category
+$xmlData.company.products.product | ForEach-Object {
+    $prodName = $_.name
+    switch ($_.category) {
+        "Accessories" { Write-Host $prodName -ForegroundColor Green }
+        "Audio" { Write-Host $prodName -ForegroundColor Yellow }
+        "Computers" { Write-Host $prodName -ForegroundColor Cyan }
+        "Electronics" { Write-Host $prodName -ForegroundColor Magenta }
+        "Storage" { Write-Host $prodName -ForegroundColor Red }
+        Default { Write-Host $prodName }
+    }
+}
+
+# Iterate all Employees in the Sales Department
+$xmlData.company.departments.department | `
+    Where-Object {$_.name -eq "Sales"} | `
+        Select-Object -ExpandProperty employee
+
+# Iterate all Employees who have 'Developer' in their title
+$xmlData.company.departments.department | `
+    Select-Object -ExpandProperty employee | `
+        Where-Object {$_.position -match "Developer"}
+
+# Iterate and Display All Employees - Display the Employee Name, Position, and Email
+$xmlData.company.departments.department | `
+    Select-Object -ExpandProperty employee | `
+        Select-Object name, position, email
+
+# Iterate and Display All Employees - Display the Employee Name, Position, Email, and Department
+$xmlData.company.departments.department | `
+    Select-Object -ExpandProperty employee | `
+        Select-Object name, position, email, @{Name="Department";Expression={$_.ParentNode.name}}
+
+# Iterate all Employees and Set the Foreground Color depending on their department
+$xmlData.company.departments.department | ForEach-Object {
+    $deptName = $_.name
+    $_.employee | ForEach-Object {
+        $empName = $_.name + " - " + $deptName
+        switch ($deptName) {
+            "Sales" { Write-Host $empName -ForegroundColor Green }
+            "IT" { Write-Host $empName -ForegroundColor Yellow }
+            "Development" { Write-Host $empName -ForegroundColor Cyan }
+            "Human Resources" { Write-Host $empName -ForegroundColor Magenta }
+            "Finance" { Write-Host $empName -ForegroundColor Red }
+            Default { Write-Host $empName }
+        }
+    }
+}
+
+# Generate an HTML Report of all Employees
+$xmlData.company.departments.department | `
+    Select-Object -ExpandProperty employee | `
+        Select-Object name, position, email, @{Name="Department";Expression={$_.ParentNode.name}} | `
+            ConvertTo-Html -Property name, position, email, Department | `
+                Out-File '.\Challenge 2\Challenge-2-Employees.html'
+
+# Launch the HTML Report
+Invoke-Item '.\Challenge 2\Challenge-2-Employees.html'
+
+# Generate an HTML Report of all Products
+$xmlData.company.products.product | `
+    Select-Object name, category, price | `
+        ConvertTo-Html -Property name, category, price | `
+            Out-File '.\Challenge 2\Challenge-2-Products.html'
+
+# Launch the HTML Report
+Invoke-Item '.\Challenge 2\Challenge-2-Products.html'
+
+# Generate an HTML Report to Display ALL Data
+function Convert-SectionToHtml {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$SectionTitle,
+
+        [Parameter(Mandatory = $true)]
+        [System.Collections.IEnumerable]$Data
+    )
+
+    "<h2>$SectionTitle</h2>" + ($Data | ConvertTo-Html -Fragment)
+}
+
+# Process departments and employees
+$departmentsHtml = Convert-SectionToHtml -SectionTitle "Departments and Employees" -Data (
+    $xmlData.company.departments.department | ForEach-Object {
+        $dept = $_
+        $_.employee | Select-Object @{Name="Department"; Expression={$dept.name}}, id, name, position, email
+    }
+)
+
+# Process products
+$productsHtml = Convert-SectionToHtml -SectionTitle "Products" -Data $xmlData.company.products.product
+
+# Combine HTML content
+$fullHtml = @"
+<html>
+<head>
+    <title>Full Company Report</title>
+    <style>
+        body { font-family: Arial, sans-serif; }
+        table { border-collapse: collapse; width: 100%; }
+        th, td { border: 1px solid #dddddd; text-align: left; padding: 8px; }
+        th { background-color: #f2f2f2; }
+    </style>
+</head>
+<body>
+    $departmentsHtml
+    $productsHtml
+</body>
+</html>
+"@
+
+# Save to file
+$fullHtml | Out-File '.\Challenge 2\Challenge-2-All.html'
+
+
+# Launch the HTML Report
+Invoke-Item '.\Challenge 2\Challenge-2-All.html'
+
+
 
 
